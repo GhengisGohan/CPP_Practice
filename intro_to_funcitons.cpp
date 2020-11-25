2f8907bed11e4e51c7f47cb175abf95828cd066c
GhengisGohan <soren.ford.anderson@gmail.com>
2020-11-24 21:52:18 -0500

    intro to functions has been updated

---
+++ intro_to_funcitons.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath> //pow didn't work as a function at first so i just include cmath and it worked
+/* 
+    example of a function: function_name(arg1, arg2) 
+*/
+
+int main()
+{
+   int x;
+   x = pow(10,2);
+   x = x*2; 
+
+   std::cout << x << std::endl;
+}
